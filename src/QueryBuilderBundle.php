c24c9464efde918bd28fd5de56dd8333953f85f4
sedliarchuk <sedliarchuk@gmail.com>
2018-06-18 13:46:06 +0300

    Add init bundle

---
+++ src/QueryBuilderBundle.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sedliarchuk
+ * Date: 18.06.2018
+ * Time: 13:41
+ */
+
+namespace Sedliarchuk\QueryBuilderBundle;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class QueryBuilderBundle extends Bundle
+{
+
+}
