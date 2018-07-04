19c2bb91df3f281dff8317bc5754c26c079a3394
sedliarchuk <sedliarchuk@gmail.com>
2018-07-04 11:11:13 +0300

    FIX join list filter

--- src/Queries/AndFilter.php
+++ src/Queries/AndFilter.php
@@ -125,6 +125,7 @@ class AndFilter
                     );
                 }
                 $whereCondition .= $value;
+                $this->conditions[] = $whereCondition;
             }
         }
 
