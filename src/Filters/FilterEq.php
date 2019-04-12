c5c7cf683b25fde3b1cc46b478b93577a2afd9ca
sedliarchuk <sedliarchuk@gmail.com>
2019-04-12 14:34:59 +0300

    Update FilterEq.php

--- src/Filters/FilterEq.php
+++ src/Filters/FilterEq.php
@@ -2,7 +2,7 @@
 /**
  * Created by PhpStorm.
  * User: sedliarchuk
- * Date: 12.09.2018
+ * Date: 12.04.2019
  * Time: 11:49
  */
 
@@ -17,34 +17,48 @@ class FilterEq extends FilterAbstract
     const FILTER_ALIAS = 'eq';
 
     function buildQueryJoin(QueryBuilder $qb) {
-        $field = $this->getQBAlias($qb) .'.'.$this->getField();
+
+        $field = $this->getField();
+        $fieldName = (explode('.', $field))[0];
+        $field = $this->getQBAlias($qb) .'.'.$fieldName;
         $tableAlias = mb_strcut($field, 0, 1).$this->getIntParameter();
         $qb->innerJoin($field, $tableAlias);
 
         $value = explode(',', $this->getValue());
-        $parameterName = $this->getField().$this->getIntParameter();
+        $parameterName = $fieldName.$this->getIntParameter();
         $qb->setParameter($parameterName, $value);
 
-        return $qb->expr()->in(
-            $tableAlias.'.id', ':'.$parameterName
-        );
+        if (!isset((explode('.', $this->getField()))[1])) {
+            return $qb->expr()->in(
+                $tableAlias.'.id', ':'.$parameterName
+            );
+        } else {
+            return $qb->expr()->in(
+                $tableAlias.'.'.(explode('.', $this->getField()))[1], ':'.$parameterName
+            );
+        }
+
     }
 
     public function buildQuery(QueryBuilder $qb, BaseRepository $repository)
     {
         $this->setRepository($repository);
+
         $field = $this->getField();
+        $fieldName = (explode('.', $field))[0];
 
         //проверяем на наличие поле в базе данных
-        if ( ! $this->issetField($field)) {
+        if ( ! $this->issetField($fieldName)) {
             return false;
         }
 
-        if ($this->isJoinField($field)) {
+        if ($this->isJoinField($fieldName)) {
             return $this->buildQueryJoin($qb);
         }
-        $field = $this->getQBAlias($qb) .'.'.$this->getField();
-        $parameterName = $this->getField().$this->getIntParameter();
+
+
+        $field = $this->getQBAlias($qb) .'.'.$fieldName;
+        $parameterName = $fieldName.$this->getIntParameter();
         $qb->setParameter($parameterName, $this->getValue());
 
         return $qb->expr()->eq(
